a42d9e3359b61253586ff5ee5e47453ff06057d0
Noor <harnoorsaini789@gmail.com>
2023-04-03 19:13:54 +0530

    Updated Connection to device

--- app/src/main/java/com/nas/smartmotor/MainActivity.java
+++ app/src/main/java/com/nas/smartmotor/MainActivity.java
@@ -2,13 +2,93 @@ package com.nas.smartmotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.telephony.SmsManager;
 
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity implements MessageListener {
+    private static final int PERMISSION_RQST_SEND = 0;
+    private static final int READ_SMS_PERMISSION_CODE = 445566;
+    EditText devicenum, phonenum, devicepin;
+    Button register, help, buy;
+    String msg,to;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(checkSelfPermission(Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getApplicationContext(), "Permission Granted!", Toast.LENGTH_LONG).show();
+        } else{
+            String[] permissionRequested={Manifest.permission.READ_SMS};
+            requestPermissions(permissionRequested, READ_SMS_PERMISSION_CODE);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        MessageReceiver.bindListener(this);
+        devicenum = (EditText) findViewById(R.id.loginDevicePhone);
+        phonenum = (EditText) findViewById(R.id.loginPhone);
+        devicepin = (EditText) findViewById(R.id.loginDevicePass);
+        register = (Button) findViewById(R.id.loginRegisterBtn);
+        help = (Button) findViewById(R.id.loginHelpBtn);
+        buy = (Button) findViewById(R.id.loginBuyBtn);
+
+        register.setOnClickListener(view -> {
+
+            //Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            //PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),0,intent,0);
+            msg = "@LK" + devicepin.getText().toString() + "&" + phonenum.getText().toString();
+            to = devicenum.getText().toString();
+
+            checkPerms();
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(to, null, msg, null, null);
+                Toast.makeText(getApplicationContext(), "Msg Sent, Waiting for Response!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Missing Permissions.", Toast.LENGTH_LONG).show();
+            }
+            /*SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(to,null,msg,null,null);
+            Toast.makeText(getApplicationContext(), "Message Sent! Waiting for Response!",
+                    Toast.LENGTH_LONG).show();
+            */
+        });
+        }
+            protected void checkPerms() {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)) {
+
+                    }
+                    else { ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_RQST_SEND);
+                    }
+                }
+            }
+            @Override
+            public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                switch (requestCode) {
+                    case PERMISSION_RQST_SEND: {
+                        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(getApplicationContext(), "Permission Granted!, Sending Message.", Toast.LENGTH_LONG).show();
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(to, null, msg, null, null);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Missing Permission to Send SMS!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            }
+            @Override
+            public void messageReceived(String message) {
+                Toast.makeText(this, "New Message Received: " + message, Toast.LENGTH_SHORT).show();
+            }
 }
